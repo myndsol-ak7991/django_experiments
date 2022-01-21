bafd84f78c26d687c4d145e1fd70b7bd34c1d887
myndsol-ak7991 <97430632+myndsol-ak7991@users.noreply.github.com>
2022-01-21 16:48:29 +0530

    Update settings.py

--- django_experiments/settings.py
+++ django_experiments/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&o=33&$k634)rso+-3j1xi_b@47pm0am8h)6uonxy)sl20r=o9'
+SECRET_KEY = 'default_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
